fbcb30601ce6bc5752efc9902e957bc021073a54
tushar6905 <mithusharma67890@gmail.com>
2025-01-26 14:15:52 +0530

    added my resume

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -11,7 +11,10 @@ const Hero = () => {
             <p className={styles.description}>
                 I'm a front-end developer who can build Websites and Hybrid Mobile Apps using React JS and React Native. Soon going to become a full-stack developer 
             </p>
+            <div>
             <a href="mailto:mithusharma67890@gmail.com"className={styles.contactBtn} target='blank'>Contact Me</a>
+            <a style={{marginLeft:10}} href="https://drive.google.com/file/d/1RNQxzbByueHG5J8FH6fTn38uKIjq9053/view"className={styles.contactBtn} target='blank'>My Resume</a>
+            </div>
         </div>
         <img src={getImageUrl("hero/heroImage.png")} alt="My Image" className={styles.heroImg} />
         <div className={styles.topBlur} />
